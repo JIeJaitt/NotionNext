e97df702cf2bd413be811fc4f50bf5b0d7447bef
JIeJaitt <jiejaitt@gmail.com>
2023-09-05 17:51:40 +0800

    refactor: delete unuseful custom js code

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,1 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-
-// 创建一个链接元素
-var linkElement = document.createElement('link');
-
-// 设置rel属性
-linkElement.rel = 'apple-touch-icon';
-
-// 设置href属性
-linkElement.href = '/favicon.ico';
-
-// 将链接元素添加到文档的头部
-document.head.appendChild(linkElement);
